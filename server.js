3a27589e6eb62376be46499222e3441f981c8b3d
Abdullah-saafi <abdullahsaafipro@gmail.com>
2026-04-14 12:23:35 +0500

    test

--- server.js
+++ server.js
@@ -40,5 +40,5 @@ app.get("", (req, res) => res.send("API running..."));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, "0.0.0.0", () =>
-  console.log(`Server running on port ${PORT}`),
+  console.log(`Server running on port this PORT ${PORT}`),
 );
